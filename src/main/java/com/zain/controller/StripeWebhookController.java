0a06e2dd5f6792880740aabc1013cf8573e4be7c
zainhanif73 <z.hanif@svg-solutions.com>
2025-01-24 12:37:31 +0100

    Change Order Status After Stripe Success Event

--- src/main/java/com/zain/controller/StripeWebhookController.java
+++ src/main/java/com/zain/controller/StripeWebhookController.java
@@ -8,6 +8,7 @@ import com.stripe.net.Webhook;
 import com.stripe.exception.SignatureVerificationException;
 import com.zain.service.OrderService;
 import lombok.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,7 +21,8 @@ public class StripeWebhookController {
 
     private String endpointSecret = "whsec_esoe9jRnxS660rdE4fGVfyI1MLTZZrYM";
 
-
+    @Autowired
+    OrderService orderService;
 
     @PostMapping("/webhook")
     public ResponseEntity<String> handleWebhook(@RequestBody String payload, @RequestHeader("Stripe-Signature") String sigHeader) {
@@ -40,20 +42,18 @@ public class StripeWebhookController {
             Session session = (Session) event.getData().getObject();
             String orderId = session.getMetadata().get("order_id");
 
-            // Validate if the orderId is present
             if (orderId == null || orderId.isEmpty()) {
                 return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Missing orderId in metadata");
             }
 
             try {
-                // Attempt to parse the orderId
                 Long parsedOrderId = Long.parseLong(orderId);
                 String paymentId = session.getPaymentIntent();
 
                 System.out.println("Payment successful: Order ID: " + parsedOrderId + ", Payment ID: " + paymentId);
-                // Update the payment status
+                orderService.updatePaymentStatus(parsedOrderId, paymentId, "COMPLETED");
+
             } catch (NumberFormatException e) {
-                // If orderId is not a valid number, return a bad request response
                 return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid orderId format: " + orderId);
             }
         }
